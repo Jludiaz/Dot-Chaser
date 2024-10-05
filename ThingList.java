64340b33192ac7e6b8d4b4690bf454a59958efa8
Jeremy <jeremyludiaz@gmail.com>
2024-10-05 19:41:33 -0400

    Created a listsinstead of nodes

--- ThingList.java
+++ ThingList.java
@@ -1,3 +1,32 @@
+import java.util.*;
+
 public class ThingList {
+
+    ArrayList<Thing>listThings = new ArrayList<>();
+    private Thing thing;
+
+    public ThingList(Thing t){
+        thing = t;
+        listThings.add(t);
+    }
+
+    public Thing getThing(){
+        return thing;
+    }
+
+    public void setThing(Thing thing){
+        this.thing = thing;
+    }
+
+    public void printAll(){
+        for (Thing t : listThings) {
+            t.toString();
+        }
+    }
+
+    public String toString(){
+        return "Thing: " + thing.row + " " + thing.col + " " + thing.lab;
+    }
+
     
 }
